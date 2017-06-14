2803076ff5efc7694e939a2566d073eb2233b697
Chris Humboldt <chrismodem@gmail.com>
2017-06-14 10:54:01 +0200

    In the process of changing stuffs.

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# RocketRequire
-Content goes here.
+# Rocket Require
+A script and stylesheet loading module.
+
+* [Getting Started](#getting-started)
+* [Full Example](#full-example)
+* [Initialisation](#initialisation)
+   * [Options](#options)
+   * [Defaults](#defaults)
+
+## Getting Started
+Install via NPM.
+
+```
+npm install rocket-require
+```
+
+**NOTE** that this module has a dependency [Rocket Tools (21kb)](https://github.com/chrishumboldt/Rocket-Tools) which will automatically be installed as well.
+
+Simply start by including the required Javascript file.
+
+```html
+<body>
+   /* Your content goes here */
+   <script src="node_modules/rocket-tools/js/tools.min.js"></script>
+   <script src="node_modules/rocket-require/js/require.min.js"></script>
+</body>
+```
+
+## Full Example
+In the example below we are attempting to load a local Rocket Button module. We can see that is a dependency on Rocket Tools, which has also been declared. As a result Rocket Tools will be loaded first.
+
+Once all files are present on the DOM a function is called displaying a message.
+
+```html
+<script>
+// Initilise a require instance
+var require = Rocket.require();
+
+// Declare the modules
+Rocket.module.add({
+   rocketButton: {
+      requires: ['rocketTools'],
+      css: ['~/rocket-button/css/button.min.css']
+      js: ['~/rocket-button/js/button.min.js']
+   },
+   rocketTools: {
+      js: ['~/rocket-tools/js/tools.min.js']
+   }
+});
+
+// Require
+require.add('rocketButton');
+
+require.load(function () {
+   console.log('Rocket Buttons and Rocket Tools have just been loaded with Rocket Require!');
+});
+</script>
+```
+
+You will notice the **~/** before all the paths. This inserts the defaults root path to all module files which by default is set to **./node_modules/**. Thus the resulting path for all files becomes:
+
+```javascript
+rocketButton: {
+   requires: ['rocketTools'],
+   css: ['./node_modules/rocket-button/css/button.min.css']
+   js: ['./node_modules/rocket-button/js/button.min.js']
+},
+rocketTools: {
+   js: ['./node_modules/rocket-tools/js/tools.min.js']
+}
+```
+
+This is just a useful shorthand feature.
+
+## Initialisation
+Before you can use the require module you first need to initialise an instance. You will then be able to manage your requirements as needed.
+
+```javascript
+var require = Rocket.require();
+```
--- build/js/require.js
+++ build/js/require.js
@@ -1,7 +1,12 @@
+/**
+@author Chris Humboldt
+**/
+// Extend Rocket
 Rocket.defaults.require = {
     errors: true,
     rootPath: './node_modules/'
 };
+// Load main file (if provided)
 function RockMod_RequireLoadMain() {
     var loadMainScript = document.querySelector('script[data-main]');
     if (loadMainScript) {
@@ -13,14 +18,23 @@ function RockMod_RequireLoadMain() {
     }
 }
 ;
+// Rocket module
 var RockMod_Module;
 (function (RockMod_Module) {
+    // Variables
     var listModules = {};
+    // Functions
+    /*
+    All module related methods go here. Makes it easier to manage,
+    especially within Typescript.
+    */
     var moduleMethods = {
         add: function (obj) {
+            // Catch
             if (!Rocket.is.object(obj)) {
                 return false;
             }
+            // Continue
             for (var key in obj) {
                 if (obj.hasOwnProperty(key)) {
                     if (validate.module(key, obj[key])) {
@@ -42,10 +56,13 @@ var RockMod_Module;
             }
         },
         dependencies: function (name) {
+            // Catch
             if (!Rocket.is.string(name)) {
                 return false;
             }
+            // Continue
             var dependencies = [];
+            // Functions
             function checkModule(name) {
                 if (moduleMethods.exists(name)) {
                     dependencies.push(name);
@@ -67,32 +84,41 @@ var RockMod_Module;
                 }
             }
             ;
+            // Execute
             checkModule(name);
             return dependencies;
         },
         exists: function (name) {
+            // Catch
             if (!Rocket.is.string(name)) {
                 return false;
             }
+            // Continue
             return Rocket.exists(listModules[name]);
         },
         get: function (name) {
+            // Catch
             if (!Rocket.is.string(name)) {
                 return false;
             }
+            // Continue
             return listModules[name];
         },
         isLoaded: function (name) {
+            // Catch
             if (!Rocket.is.string(name)) {
                 return false;
             }
+            // Continue
             var thisModule = listModules[name];
             return (Rocket.is.object(thisModule)) ? thisModule.loaded : false;
         },
         isLoading: function (name) {
+            // Catch
             if (!Rocket.is.string(name)) {
                 return false;
             }
+            // Continue
             var thisModule = listModules[name];
             return (Rocket.is.object(thisModule)) ? thisModule.loading : false;
         },
@@ -106,15 +132,19 @@ var RockMod_Module;
             return listLoaded;
         },
         remove: function (name) {
+            // Catch
             if (!Rocket.is.string(name) || !Rocket.exists(listModules[name])) {
                 return false;
             }
+            // Continue
             delete listModules[name];
         },
         sanitisePaths: function (paths) {
+            // Convert string to array
             if (Rocket.is.string(paths)) {
                 paths = [paths];
             }
+            // Set the root url if need be
             for (var len = paths.length, i = 0; i < len; i++) {
                 paths[i] = paths[i].replace(/~\//g, Rocket.defaults.require.rootPath);
             }
@@ -150,6 +180,7 @@ var RockMod_Module;
             return true;
         }
     };
+    // Exports
     RockMod_Module.add = moduleMethods.add;
     RockMod_Module.exists = moduleMethods.exists;
     RockMod_Module.get = moduleMethods.get;
@@ -160,13 +191,16 @@ var RockMod_Module;
     RockMod_Module.loaded = moduleMethods.listLoaded;
     RockMod_Module.remove = moduleMethods.remove;
 })(RockMod_Module || (RockMod_Module = {}));
+// Rocket require
 var RockMod_Require;
 (function (RockMod_Require) {
+    // Functions
     function loadFile(file, callback, customRootPath) {
         var theInclude;
         var type;
         var rootUrl = (Rocket.is.string(customRootPath)) ? customRootPath : '';
         var filePath = (Rocket.is.url(file)) ? file : rootUrl + file;
+        // Create include element
         if (/(.css)$/.test(file)) {
             type = 'css';
             theInclude = document.createElement('link');
@@ -179,6 +213,7 @@ var RockMod_Require;
             theInclude.setAttribute('async', true);
             theInclude.src = filePath;
         }
+        // Listen for completion
         theInclude.onload = function () {
             if (type !== 'js' && Object.hasOwnProperty.call(window, "ActiveXObject") && !window['ActiveXObject']) {
                 if (Rocket.is.function(callback)) {
@@ -207,25 +242,32 @@ var RockMod_Require;
         };
         document.getElementsByTagName('head')[0].appendChild(theInclude);
     }
+    // Load module files
     function loadModuleFiles(thisModule, callback) {
         var count = 0;
         var files = [];
+        // CSS
         if (Rocket.is.string(thisModule.css)) {
             files.push(thisModule.css);
         }
         else if (Rocket.is.array(thisModule.css) && thisModule.css.length > 0) {
             files = files.concat(thisModule.css);
         }
+        // JS
         if (Rocket.is.string(thisModule.js)) {
             files.push(thisModule.js);
         }
         else if (Rocket.is.array(thisModule.js) && thisModule.js.length > 0) {
             files = files.concat(thisModule.js);
         }
+        // Count
         count = files.length;
+        // Execute
+        // Catch
         if (count < 1) {
             return callback();
         }
+        // Continue
         for (var _i = 0, files_1 = files; _i < files_1.length; _i++) {
             var file = files_1[_i];
             loadFile(file, function (response) {
@@ -237,22 +279,35 @@ var RockMod_Require;
             }, false);
         }
     }
+    // Require instance
+    /*
+    Self contain the require so that each instance has its own scope.
+    */
     var Require = (function () {
         function Require() {
             this.modules = [];
         }
+        // Functions
         Require.prototype.add = function (name) {
+            // Check
             if (!Rocket.is.string(name)
                 || !Rocket.module.exists(name)
                 || Rocket.module.isLoaded(name)
                 || this.modules.indexOf(name) > -1) {
                 return false;
             }
+            // Continue
             this.modules = this.modules.concat(Rocket.module.dependencies(name));
             this.modules = Rocket.array.unique(this.modules);
         };
+        /*
+        The load function will take the newly created require instance and execute
+        the loading of each module in the various dependency stacks.
+        */
         Require.prototype.load = function (callback) {
+            // Variables
             var self = this;
+            // Functions
             function loadExecute() {
                 loadModules(self.modules, function () {
                     self.modules = [];
@@ -262,6 +317,11 @@ var RockMod_Require;
                 });
             }
             function loadModule(name, callback) {
+                /*
+                Check to see if the module exists. Rocket require is explicit and will
+                hard fail should a module not be found. This is to protect against lazy
+                module management.
+                */
                 if (!Rocket.module.exists(name)) {
                     if (Rocket.defaults.require.errors) {
                         throw new Error('ROCKET REQUIRE: You are missing a required module: ' + name);
@@ -269,11 +329,16 @@ var RockMod_Require;
                 }
                 else {
                     var thisModule_1 = Rocket.module.get(name);
+                    /*
+                    Here we find that the module is already loaded or loading.
+                    Return the callback and move on as needed.
+                    */
                     if (thisModule_1.loaded || thisModule_1.loading) {
                         if (thisModule_1.loaded) {
                             return callback();
                         }
                         else {
+                            // Poll the variable once loaded
                             var modIntervalCheck_1 = setInterval(function () {
                                 if (thisModule_1.loaded) {
                                     clearInterval(modIntervalCheck_1);
@@ -284,7 +349,9 @@ var RockMod_Require;
                     }
                     else {
                         var dependencies = (Rocket.is.array(thisModule_1.requires) && thisModule_1.requires.length > 0) ? thisModule_1.requires : false;
+                        // Change state to loading
                         thisModule_1.loading = true;
+                        // Check dependency
                         if (!dependencies) {
                             return loadModuleFiles(thisModule_1, callback);
                         }
@@ -308,6 +375,7 @@ var RockMod_Require;
                     });
                 }
             }
+            // Execute
             loadExecute();
         };
         return Require;
@@ -315,8 +383,10 @@ var RockMod_Require;
     RockMod_Require.newRequire = Require;
     RockMod_Require.load = loadFile;
 })(RockMod_Require || (RockMod_Require = {}));
+// Bind to the Rocket object
 Rocket.module = RockMod_Module;
 Rocket.require = function () {
     return new RockMod_Require.newRequire;
 };
+// Load main file
 RockMod_RequireLoadMain();
